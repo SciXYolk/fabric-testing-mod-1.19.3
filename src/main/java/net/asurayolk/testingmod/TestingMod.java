2e35e897e42015d61f74a0f1f062ba6565e93299
DingoBerry <cameleongaming07@gmail.com>
2023-07-07 09:33:09 +0100

    gaga

--- src/main/java/net/asurayolk/testingmod/TestingMod.java
+++ src/main/java/net/asurayolk/testingmod/TestingMod.java
@@ -1,5 +1,6 @@
 package net.asurayolk.testingmod;
 
+import net.asurayolk.testingmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,7 +12,6 @@ public class TestingMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/asurayolk/testingmod/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.asurayolk.testingmod.item;
+
+import net.asurayolk.testingmod.TestingMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item CITRINE = registerItem("citrine",
+            new Item(new FabricItemSettings()));
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(TestingMod.MOD_ID, name), item);
+    }
+
+    public static void addItemsToItemGroup(){
+        addToItemGroup(ItemGroups.INGREDIENTS, CITRINE);
+    }
+    private static void addToItemGroup(ItemGroup group, Item item){
+        ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
+    }
+    public static void registerModItems(){
+        TestingMod.LOGGER.info("Registering Mod Items for " + TestingMod.MOD_ID);
+
+        addItemsToItemGroup();
+    }
+}
