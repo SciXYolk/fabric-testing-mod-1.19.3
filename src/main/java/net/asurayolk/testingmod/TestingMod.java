d7376df69eea905ac9a5b1fe24a82d3956c905d6
DingoBerry <cameleongaming07@gmail.com>
2023-07-07 08:51:00 +0100

    gaga

--- src/main/java/net/asurayolk/testingmod/TestingMod.java
+++ src/main/java/net/asurayolk/testingmod/TestingMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//dada
 public class TestingMod implements ModInitializer {
 	public static final String MOD_ID = "testingmod";
     public static final Logger LOGGER = LoggerFactory.getLogger("testingmod");
